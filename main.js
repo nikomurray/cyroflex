7411d35976f10788f07631e72be23411af9965c5
Nicolas <npereyramurray@gmail.com>
2024-04-28 23:10:05 -0300

    finish workflow section

--- main.js
+++ main.js
@@ -2,22 +2,45 @@ const dropBtn = document.getElementById("dropBtn")
 
 
 
+// const methods = [
+//     { id: "method1", text: "<p>Diariamente, recolectamos los pedidos de tus ventas realizadas hasta horario de corte. Recolectamos los pedidos ya preparados, embalados y etiquetados para ser escaneados y contabilizados al momento de la recolección.<br><br>*El horario de corte generalmente es a las 12 del mediodía, nosotros ofrecemos horario de corte a las 12, 13 y 14hs* </p>" },
+//     { id: "method2", text: "<p>Organizamos todos los pedidos en nuestro centro de almacenamiento, Donde generamos diferentes rutas de distribución, para resolver todas las entregas en el día.<br><br>Este paso es fundamental para optimizar al máximo los tiempos de entrega.</p>" },
+//     { id: "method3", text: "<p>Tus paquetes parten a destino en diferentes vehículos, clasificados y ruteados por las diferentes zonas de entrega.<br><br> Esto nos permite brindar un servicio ágil y de calidad a muy bajo costo, aunque tus pedidos se encuentren muy alejados el uno del otro.</p>" },
+//     { id: "method4", text: "<p>Referencias en tiempo real del estado de tus envíos.<br><br> Además, contás con nuestros canales de atención inmediata via WhatsApp.<p>" }
+//   ];
+  
+//   const workText = document.getElementById("workText");
+  
+//   methods.forEach(method => {
+//     const element = document.getElementById(method.id);
+
+//     element.addEventListener("click", () => {
+//       workText.innerHTML = method.text;
+//     });
+//   });
+
+
 const methods = [
-    { id: "method1", text: "<p>Diariamente, recolectamos los pedidos de tus ventas realizadas hasta horario de corte. Recolectamos los pedidos ya preparados, embalados y etiquetados para ser escaneados y contabilizados al momento de la recolección.<br><br>*El horario de corte generalmente es a las 12 del mediodía, nosotros ofrecemos horario de corte a las 12, 13 y 14hs* </p>" },
-    { id: "method2", text: "<p>Organizamos todos los pedidos en nuestro centro de almacenamiento, Donde generamos diferentes rutas de distribución, para resolver todas las entregas en el día.<br><br>Este paso es fundamental para optimizar al máximo los tiempos de entrega.</p>" },
-    { id: "method3", text: "<p>Tus paquetes parten a destino en diferentes vehículos, clasificados y ruteados por las diferentes zonas de entrega.<br><br> Esto nos permite brindar un servicio ágil y de calidad a muy bajo costo, aunque tus pedidos se encuentren muy alejados el uno del otro.</p>" },
-    { id: "method4", text: "<p>Referencias en tiempo real del estado de tus envíos.<br><br> Además, contás con nuestros canales de atención inmediata via WhatsApp.<p>" }
+    { id: "method1", text: "<p>Diariamente, recolectamos los pedidos de tus ventas realizadas hasta horario de corte. Recolectamos los pedidos ya preparados, embalados y etiquetados para ser escaneados y contabilizados al momento de la recolección.<br><br>*El horario de corte generalmente es a las 12 del mediodía, nosotros ofrecemos horario de corte a las 12, 13 y 14hs* </p>", imgSrc: "./recoleccion.jpg" },
+    { id: "method2", text: "<p>Organizamos todos los pedidos en nuestro centro de almacenamiento, Donde generamos diferentes rutas de distribución, para resolver todas las entregas en el día.<br><br>Este paso es fundamental para optimizar al máximo los tiempos de entrega.</p>", imgSrc: "./distribucion.jpg" },
+    { id: "method3", text: "<p>Tus paquetes parten a destino en diferentes vehículos, clasificados y ruteados por las diferentes zonas de entrega.<br><br> Esto nos permite brindar un servicio ágil y de calidad a muy bajo costo, aunque tus pedidos se encuentren muy alejados el uno del otro.</p>", imgSrc: "./expedicion.jpg" },
+    { id: "method4", text: "<p>Referencias en tiempo real del estado de tus envíos.<br><br> Además, contás con nuestros canales de atención inmediata via WhatsApp.<p>", imgSrc: "./soporte.jpg" }
   ];
   
+  const workTextTitle = document.getElementById("workTextTitle");
   const workText = document.getElementById("workText");
+  const workImage = document.getElementById("workImg")
   
   methods.forEach(method => {
     const element = document.getElementById(method.id);
-
+  
     element.addEventListener("click", () => {
+      workTextTitle.textContent = element.textContent;
       workText.innerHTML = method.text;
+      workImage.src = method.imgSrc;
     });
   });
+  
   
 
 
